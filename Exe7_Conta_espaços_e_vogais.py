ce7ed4e5a9dd91c53d0fd5e6165a45e699835a11
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-30 09:17:49 -0300

    Update Exe7_Conta_espaços_e_vogais.py

--- Exe7_Conta_espaços_e_vogais.py
+++ Exe7_Conta_espaços_e_vogais.py
@@ -1,2 +1,15 @@
+def String(frase):
+    print(f"A frase digitada foi: {frase}")
+    print(f"Contem" , frase.count(" "), "espaços em branco")
+    print("A vogal a apareceu ",frase.count("a"), "vezes")
+    print("A vogal e apareceu ",frase.count("e"), "vezes")
+    print("A vogal i apareceu ",frase.count("i"), "vezes")
+    print("A vogal o apareceu ",frase.count("o"), "vezes")
+    print("A vogal u apareceu ",frase.count("u"), "vezes")
+
+
+
 frase = str(input("Digite uma frase qualquer: "))
-print(fraseo ra)
+String(frase)
+
+
