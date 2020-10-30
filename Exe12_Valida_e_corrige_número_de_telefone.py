4049bf4b86fb721f0158f64cbfc5e82aad1a6223
Lucas Samofalov Diniz <71664028+lucaslk122@users.noreply.github.com>
2020-10-30 13:03:33 -0300

    Update Exe12_Valida_e_corrige_número_de_telefone.py

--- Exe12_Valida_e_corrige_número_de_telefone.py
+++ Exe12_Valida_e_corrige_número_de_telefone.py
@@ -1,7 +1,23 @@
-print("Valida e corrige número de telefone")
-telefone = input("Telefone: ")
-print(len(telefone))
-if len(telefone) == 7:
-    print("Telefone possui 7 dígitos. Vou acrescentar o digito três na frente")
-    telefone_corrigido = telefone[0] = 3
-    print(f"Telefone corrigido: {telefone_corrigido}")
+n_telefone = str(input("Digite o n° do telefone: "))
+while len(n_telefone) != 8 or n_telefone != 7:
+    print("O número precisa ter 8 ou 7 digitos")
+    n_telefone = str(input("Digite o n° do telefone: "))
+numero_correto = []
+if n_telefone[3] == '-' and len(n_telefone) == 8:
+    numero_correto = ['3']
+    numero_correto.append(n_telefone)
+    numero_final = numero_correto[0] + numero_correto[1]
+    print(numero_final)
+elif '-' not in n_telefone and len(n_telefone) == 8:
+    n1 = n_telefone[0:4]
+    n2 = n_telefone[4:8]
+    print(n1 + '-' + n2)
+elif '-' not in n_telefone and len(n_telefone) == 7:
+    numero_correto = ['3']
+    numero_correto.append(n_telefone)
+    numero_final = numero_correto[0] + numero_correto[1]
+    n1 = numero_final[0:4]
+    n2 = numero_final[4:8]
+    print(n1 + '-' + n2)
+else:
+    print(n_telefone)
